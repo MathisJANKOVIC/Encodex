cf7e64cbec7f00c9b5f2f13c254c05a25e3c58d8
MathisJANKOVIC <mathis.jankovic@gmail.com>
2023-12-02 16:19:54 +0100

    add base route to endpoints

--- main.py
+++ main.py
@@ -2,30 +2,30 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/encoding")
+@app.get("/codex/")
 def get_all():
     pass
 
-@app.get("/encoding/{encoding_name}")
+@app.get("/codex/{encoding_name}")
 def get(encoding_name: str):
     pass
 
-@app.get("/encoding/{encoding_name}/encode/{sequence}")
+@app.get("/codex/{encoding_name}/encode/{sequence}")
 def encode(encoding_name: str, sequence: str):
     pass
 
-@app.get("/encoding/{encoding_name}/decode/{sequence}")
+@app.get("/codex/{encoding_name}/decode/{sequence}")
 def decode(encoding_name: str, sequence: str):
     pass
 
-@app.patch("/encoding/{encoding_name}/update/{new_encoding_format}")
+@app.patch("/codex/{encoding_name}/update/{new_encoding_format}")
 def update(encoding_name: str, new_encoding_format: dict):
     pass
 
-@app.delete("/encoding/{encoding_name}/delete/")
+@app.delete("/codex/{encoding_name}/delete/")
 def delete(encoding_name: str):
     pass
 
-@app.post("/encoding/create/{encoding_name}")
+@app.post("/codex/create/{encoding_name}")
 def create(encoding_name: str):
     pass
