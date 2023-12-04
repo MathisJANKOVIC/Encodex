64c6bc4c9703b920ece521c71d3b1a5d89aee806
unknown <mathis.jankovic@gmail.com>
2023-12-04 23:59:38 +0100

    add constructors

--- api/models.py
+++ api/models.py
@@ -5,22 +5,31 @@ import database_setup
 
 Base = declarative_base()
 
+class EncodingCharacter(Base):
+    __tablename__ = 'encoding_character'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+
+    char = Column(String(1), nullable=False)
+    encoded_char = Column(String(1), nullable=False)
+
+    id_type = Column(Integer, ForeignKey('encoding_type.id'), nullable=False)
+
+    def __init__(self, char: str, encoded_char: str):
+        self.char = char
+        self.encoded_char = encoded_char
+
 class EncodingType(Base):
     __tablename__ = 'encoding_type'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(255), nullable=False)
+    encoding_chars = relationship('EncodingCharacter', backref='encoding_type')
 
-    encoding_characters = relationship('EncodingCharacters', backref='encoding_type')
+    def __init__(self, name: str, encoding_characters: dict):
+        self.name = name
 
-class EncodingCharacters(Base):
-    __tablename__ = 'encoding_characters'
-
-    id = Column(Integer, autoincrement=True, primary_key=True)
-
-    character = Column(String(1), nullable=False)
-    encoded_character = Column(String(1), nullable=False)
-
-    id_type = Column(Integer, ForeignKey('encoding_type.id'), nullable=False)
+        for char, encoding_char in encoding_characters.items():
+            self.encoding_chars.append(EncodingCharacter(char, encoding_char))
 
 Base.metadata.create_all(database_setup.engine)
