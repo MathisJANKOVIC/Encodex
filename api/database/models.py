00037067024db237189f19aee443e01ff90085ac
unknown <mathis.jankovic@gmail.com>
2023-12-06 12:28:58 +0100

    fix import connection error & db structure primary key bug

--- api/database/models.py
+++ api/database/models.py
@@ -1,19 +1,19 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, declarative_base
 
-import connection
+from database import connection
 
 Base = declarative_base()
 
 class EncodingCharacter(Base):
     __tablename__ = 'encoding_character'
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
 
     char = Column(String(1), nullable=False)
     encoded_char = Column(String(255), nullable=False)
 
-    id_type = Column(Integer, ForeignKey('encoding_type.id'), nullable=False)
+    id_encoding_type = Column(Integer, ForeignKey('encoding_type.id'), nullable=False)
 
     def __init__(self, char: str, encoded_char: str):
         self.char = char
@@ -22,7 +22,9 @@ class EncodingCharacter(Base):
 class EncodingType(Base):
     __tablename__ = 'encoding_type'
 
-    name = Column(String(255), nullable=False, unique=True, primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(255), nullable=False)
+
     encoding_chars = relationship('EncodingCharacter', backref='encoding_type')
 
     def __init__(self, name: str, encoding_characters: dict):
