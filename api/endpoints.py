cb1f4e1d25a665dfa92bace6904bbd0989c1bed2
unknown <mathis.jankovic@gmail.com>
2023-12-06 20:35:37 +0100

    rename base routes

--- api/endpoints.py
+++ api/endpoints.py
@@ -2,30 +2,30 @@ from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-@app.get("/codex/")
+@app.get("/encodex/")
 def get_all():
     pass
 
-@app.get("/codex/{encoding_name}")
+@app.get("/encodex/{encoding_name}")
 def get(encoding_name: str):
     pass
 
-@app.get("/codex/{encoding_name}/encode/{sequence}")
+@app.get("/encodex/{encoding_name}/encode/{sequence}")
 def encode(encoding_name: str, sequence: str):
     pass
 
-@app.get("/codex/{encoding_name}/decode/{sequence}")
+@app.get("/encodex/{encoding_name}/decode/{sequence}")
 def decode(encoding_name: str, sequence: str):
     pass
 
-@app.patch("/codex/{encoding_name}/update/")
+@app.patch("/encodex/{encoding_name}/update/")
 def update(encoding_name: str, new_encoding_format: dict = Body(...)):
     pass
 
-@app.delete("/codex/{encoding_name}/delete/")
+@app.delete("/encodex/{encoding_name}/delete/")
 def delete(encoding_name: str):
     pass
 
-@app.post("/codex/create/{encoding_name}")
+@app.post("/encodex/create/{encoding_name}")
 def create(encoding_name: str):
     pass
