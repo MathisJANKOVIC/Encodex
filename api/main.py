382aca41dea7860e2a281bac70cbada5eb04bbfe
unknown <mathis.jankovic@gmail.com>
2023-12-06 22:28:46 +0100

    implement delete route

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException, Body
 
-from database.models import EncodingType
+from database.models import EncodingCharacter, EncodingType
 from database.connection import LocalSession
 
 app = FastAPI()
@@ -25,9 +25,23 @@ def decode(encoding_name: str, sequence: str):
 def update(encoding_name: str, new_encoding_format: dict = Body(...)):
     pass
 
-@app.delete("/encodex/{encoding_name}/delete/")
-def delete(encoding_name: str):
-    pass
+@app.delete("/encodex/{encoding_type_name}/delete/", status_code=200)
+def delete(encoding_type_name: str):
+
+    session = LocalSession()
+    encoding_type = session.query(EncodingType).filter(EncodingType.name == encoding_type_name).first()
+
+    if(encoding_type is None):
+        session.close()
+        raise HTTPException(status_code=404, detail={"succes": False, "message": "Encoding type not found"})
+
+    session.query(EncodingCharacter).filter(EncodingCharacter.id_encoding_type == encoding_type.id).delete()
+    session.delete(encoding_type)
+
+    session.commit()
+    session.close()
+
+    return {"succes": True, "message": "Encoding type deleted successfully"}
 
 @app.post("/encodex/create/{encoding_type_name}", status_code=201)
 def create(encoding_type_name: str, encoding_characters: dict = Body(...)):
@@ -41,4 +55,5 @@ def create(encoding_type_name: str, encoding_characters: dict = Body(...)):
     session.add(EncodingType(encoding_type_name, encoding_characters))
     session.commit()
 
-    return {"Succes": True, "message": "Encoding type created successfully"}
+    session.close()
+    return {"succes": True, "message": "Encoding type created successfully"}
