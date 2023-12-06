27a10f3ef7791c85075208ec7a54d2ac5be0329b
unknown <mathis.jankovic@gmail.com>
2023-12-06 20:17:43 +0100

    make table names plural & make name attr unique

--- api/database/models.py
+++ api/database/models.py
@@ -6,26 +6,26 @@ from database import connection
 Base = declarative_base()
 
 class EncodingCharacter(Base):
-    __tablename__ = 'encoding_character'
+    __tablename__ = 'encoding_characters'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
 
     char = Column(String(1), nullable=False)
     encoded_char = Column(String(255), nullable=False)
 
-    id_encoding_type = Column(Integer, ForeignKey('encoding_type.id'), nullable=False)
+    id_encoding_type = Column(Integer, ForeignKey('encoding_types.id'), nullable=False)
 
     def __init__(self, char: str, encoded_char: str):
         self.char = char
         self.encoded_char = encoded_char
 
 class EncodingType(Base):
-    __tablename__ = 'encoding_type'
+    __tablename__ = 'encoding_types'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
-    name = Column(String(255), nullable=False)
+    name = Column(String(255), nullable=False, unique=True)
 
-    encoding_chars = relationship('EncodingCharacter', backref='encoding_type')
+    encoding_chars = relationship('EncodingCharacter', backref='encoding_types')
 
     def __init__(self, name: str, encoding_characters: dict):
         self.name = name
