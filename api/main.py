000af62cf8886c571b0c7d4754dc6ebda5a438e3
unknown <mathis.jankovic@gmail.com>
2023-12-07 01:14:38 +0100

    implement get encoding type routes

--- api/main.py
+++ api/main.py
@@ -1,18 +1,45 @@
 from fastapi import FastAPI, Body
 from fastapi.responses import JSONResponse
 
-from database.models import EncodingCharacter, EncodingType
 from database.connection import LocalSession
+from database.models import EncodingCharacter, EncodingType
 
 app = FastAPI()
 
 @app.get("/encodex/")
 def get_all():
-    pass
 
-@app.get("/encodex/{encoding_name}")
-def get(encoding_name: str):
-    pass
+    session = LocalSession()
+    encoding_types = session.query(EncodingType).all()
+
+    encoding_types = [{
+            "id": encoding_type.id,
+            "name": encoding_type.name,
+            "encoding_chars": {encoding_char.char: encoding_char.encoded_char for encoding_char in encoding_type.encoding_chars}
+        } for encoding_type in encoding_types
+    ]
+
+    session.close()
+    return JSONResponse(status_code=200, content={"succes": True, "content": encoding_types})
+
+@app.get("/encodex/{encoding_type_name}")
+def get(encoding_type_name: str):
+
+    session = LocalSession()
+    encoding_type = session.query(EncodingType).filter(EncodingType.name == encoding_type_name).first()
+
+    if(encoding_type is None):
+        session.close()
+        return JSONResponse(status_code=404, content={"succes": False, "message": "Encoding type not found"})
+
+    encoding_type = {
+        "id": encoding_type.id,
+        "name": encoding_type.name,
+        "encoding_chars": {encoding_char.char: encoding_char.encoded_char for encoding_char in encoding_type.encoding_chars}
+    }
+
+    session.close()
+    return JSONResponse(status_code=200, content = {"succes": True, "content": encoding_type})
 
 @app.get("/encodex/{encoding_type_name}/encode/{sequence}", status_code=200)
 def encode(encoding_type_name: str, sequence: str):
@@ -21,6 +48,7 @@ def encode(encoding_type_name: str, sequence: str):
     encoding_type = session.query(EncodingType).filter(EncodingType.name == encoding_type_name).first()
 
     if(encoding_type is None):
+        session.close()
         return JSONResponse(status_code=404, content={"succes": False, "message": "Encoding type not found"})
 
     encoding_chars: list[EncodingCharacter] = encoding_type.encoding_chars
@@ -35,10 +63,7 @@ def encode(encoding_type_name: str, sequence: str):
         else:
             encoded_sequence = encoded_sequence + char
 
-    return JSONResponse(
-        status_code = 200,
-        content = {"succes": True, "message": "Encoding type updated successfully", "content": encoded_sequence}
-    )
+    return JSONResponse(status_code=200, content = {"succes": True, "content": encoded_sequence})
 
 @app.patch("/encodex/{encoding_type_name}/update/")
 def update(encoding_type_name: str, new_encoding_chars: dict = Body(...)):
