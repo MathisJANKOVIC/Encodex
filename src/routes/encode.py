d5bf5e6d5b234502b0c48c367bfdad3c2d91837f
MathisJANKOVIC <mathis.jankovic@gmail.com>
2023-12-13 01:36:23 +0100

    add pydantic model & apply word & chars seps

--- src/routes/encode.py
+++ src/routes/encode.py
@@ -1,31 +1,38 @@
-from fastapi import APIRouter
+from pydantic import BaseModel
+from fastapi import APIRouter, Body
 from fastapi.responses import JSONResponse
 
-from database.models import EncodingType, EncodingChar
+from database.models import EncodingType
 from database.connection import LocalSession
+
+class BodyModel(BaseModel):
+    sequence: str
 
 router = APIRouter()
 
-@router.get("/encodex/{encoding_type_name}/encode/{sequence}", status_code=200)
-def encode(encoding_type_name: str, sequence: str):
+@router.post("/encodex/{encoding_type_name}/encode/")
+def encode(encoding_type_name: str, body: BodyModel = Body(...)):
 
     session = LocalSession()
-    encoding_type = session.query(EncodingType).filter(EncodingType.name == encoding_type_name).first()
+    encoding_type: EncodingType = session.query(EncodingType).filter(EncodingType.name == encoding_type_name).first()
 
     if(encoding_type is None):
         session.close()
         return JSONResponse(status_code=404, content={"succes": False, "message": "Encoding type not found"})
 
-    encoding_chars: list[EncodingChar] = encoding_type.encoding_chars
-    session.close()
-
     encoded_sequence = ""
-    for char in sequence:
-        for encoding_char in encoding_chars:
-            if(encoding_char.char == char):
-                encoded_sequence = encoded_sequence + encoding_char.encoded_char
-                break
+    for char in body.sequence:
+        if(char == " "):
+            encoded_sequence = encoded_sequence + encoding_type.encoded_words_sep
         else:
-            encoded_sequence = encoded_sequence + char
+            encoded_char = next((encoding_char.encoded_char for encoding_char in encoding_type.encoding_chars if encoding_char.char == char), None)
+            if(encoded_char is None):
+                encoded_sequence = encoded_sequence + char
+            else:
+                encoded_sequence = encoded_sequence + encoded_char
 
-    return JSONResponse(status_code=200, content = {"succes": True, "content": encoded_sequence})
+            if(char != body.sequence[-1]):
+                encoded_sequence = encoded_sequence + encoding_type.encoded_chars_sep
+
+    session.close()
+    return JSONResponse(status_code=200, content={"succes": True, "content": encoded_sequence})
