14bd767aef41020739fbd5b13edf7aa82be4cb26
MathisJANKOVIC <mathis.jankovic@gmail.com>
2023-12-17 14:13:54 +0100

    change names and route path for delete.py

--- src/routes/delete.py
+++ src/routes/delete.py
@@ -2,21 +2,21 @@ from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
 from database.connection import LocalSession
-from database.models import EncodingType, EncodingChar
+from database.models import EncodingStandard, CodePoint
 
 router = APIRouter()
 
-@router.delete("/encodex/{encoding_type_name}/delete/")
-def delete_encoding_type(encoding_type_name: str):
+@router.delete("/encodex/delete/{encoding_type_name}")
+def delete_encoding_standard(encoding_type_name: str):
 
     session = LocalSession()
-    encoding_type = session.query(EncodingType).filter(EncodingType.name == encoding_type_name).first()
+    encoding_type = session.query(EncodingStandard).filter(EncodingStandard.name == encoding_type_name).first()
 
     if(encoding_type is None):
         session.close()
         return JSONResponse(status_code=404, content={"succes": False, "message": "Encoding type not found"})
 
-    session.query(EncodingChar).filter(EncodingChar.encoding_type_id == encoding_type.id).delete()
+    session.query(CodePoint).filter(CodePoint.encoding_standard_id == encoding_type.id).delete()
     session.delete(encoding_type)
 
     session.commit()
