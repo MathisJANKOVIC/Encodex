258e6aea925ef86c7c5039f412d4257b2b7ab461
MathisJANKOVIC <mathis.jankovic@gmail.com>
2023-12-17 02:27:38 +0100

    change naming structure & add attr allowed_unreferenced_chars

--- src/database/models.py
+++ src/database/models.py
@@ -5,63 +5,75 @@ from database import connection
 
 Base = declarative_base()
 
-class EncodingChar(Base):
-    __tablename__ = 'encoding_characters'
+class CodePoint(Base):
+    __tablename__ = 'code_points'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
 
     char = Column(String(1), nullable=False)
     encoded_char = Column(String(255), nullable=False)
 
-    encoding_type_id = Column(Integer, ForeignKey('encoding_types.id'), nullable=False)
+    encoding_standard_id = Column(Integer, ForeignKey('char_encoding_standards.id'), nullable=False)
 
     def __init__(self, char: str, encoded_char: str):
         self.char = char
         self.encoded_char = encoded_char
 
-class EncodingType(Base):
-    __tablename__ = 'encoding_types'
+class EncodingStandard(Base):
+    __tablename__ = 'char_encoding_standards'
 
     id = Column(Integer, autoincrement=True, primary_key=True)
     name = Column(String(255), nullable=False, unique=True)
-    is_case_sensitive = Column(Boolean, nullable=False)
 
-    encoded_chars_sep = Column(String(255), nullable=True)
-    encoded_words_sep = Column(String(255), nullable=True)
-    encoded_chars_len = Column(Integer, nullable=True)
+    case_sensitive = Column(Boolean, default=True)
+    allowed_unrefenced_chars = Column(Boolean, default=False)
 
-    encoding_chars = relationship('EncodingChar', backref='encoding_types')
+    encoded_char_len = Column(Integer)
+    encoded_char_sep = Column(String(255), default="")
+    encoded_word_sep = Column(String(255), default=" ")
 
-    def __init__(self, name: str, encoding_chars: dict, is_case_sensitive: bool, encoded_chars_sep: str, encoded_words_sep: str, encoded_chars_len: int | None):
+    charset = relationship('CodePoint', backref='char_encoding_standards')
+
+    def __init__(self,
+            name: str,
+            charset: dict[str, str],
+            case_sensitive: bool,
+            allowed_unrefenced_chars: bool,
+            encoded_chars_len: int | None,
+            encoded_chars_sep: str,
+            encoded_words_sep: str
+        ):
         self.name = name
-        for char, encoding_char in encoding_chars.items():
-            self.encoding_chars.append(EncodingChar(char, encoding_char))
+        for char, encoding_char in charset.items():
+            self.charset.append(CodePoint(char, encoding_char))
 
-        self.is_case_sensitive = is_case_sensitive
+        self.case_sensitive = case_sensitive
+        self.allowed_unrefenced_chars = allowed_unrefenced_chars
 
-        self.encoded_chars_sep = encoded_chars_sep
-        self.encoded_words_sep = encoded_words_sep
-        self.encoded_chars_len = encoded_chars_len
+        self.encoded_char_len = encoded_chars_len
+        self.encoded_char_sep = encoded_chars_sep
+        self.encoded_word_sep = encoded_words_sep
 
     def dict(self) -> dict:
-        """Returns a dictionary representation of EncodingType object"""
+        """Returns a dictionary representation of the object"""
         return {
             "id": self.id,
             "name": self.name,
-            "is_case_sensitive": self.is_case_sensitive,
-            "encoded_chars_sep": self.encoded_chars_sep,
-            "encoded_words_sep": self.encoded_words_sep,
-            "encoded_chars_len": self.encoded_chars_len,
-            "encoding_chars": {encoding_char.char: encoding_char.encoded_char for encoding_char in self.encoding_chars}
+            "case_sensitive": self.case_sensitive,
+            "allowed_unrefenced_chars": self.allowed_unrefenced_chars,
+            "encoded_chars_len": self.encoded_char_len,
+            "encoded_chars_sep": self.encoded_char_sep,
+            "encoded_words_sep": self.encoded_word_sep,
+            "encoding_chars": {encoding_char.char: encoding_char.encoded_char for encoding_char in self.charset}
         }
 
-    def encoded_char(self, char: str) -> str:
-        """Returns the encoded version of `char` if exists in the encoding character set otherwise returns `char`"""
-        return next((encoding_char.encoded_char for encoding_char in self.encoding_chars if encoding_char.char == char), char)
+    def encoded_char(self, char: str) -> str | None:
+        """Returns the encoded version of `char` if exists in the charset otherwise returns None"""
+        return next((encoding_char.encoded_char for encoding_char in self.charset if encoding_char.char == char), None)
 
-    def decoded_char(self, encoded_char: str) -> str:
-        """Returns the decoded version of `decoded_char` if exists in the encoding character set otherwise returns `decoded_char`"""
-        return next((encoding_char.char for encoding_char in self.encoding_chars if encoding_char.encoded_char == encoded_char), encoded_char)
+    def decoded_char(self, encoded_char: str) -> str | None:
+        """Returns the encoded version of `encoded_char` if exists in the charset otherwise returns None"""
+        return next((encoding_char.char for encoding_char in self.charset if encoding_char.encoded_char == encoded_char), None)
 
 if(__name__ == '__main__'):
     Base.metadata.drop_all(connection.engine)
