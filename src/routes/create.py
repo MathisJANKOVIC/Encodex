92320da834448be78fbf0d6b199841e8ade75459
MathisJANKOVIC <mathis.jankovic@gmail.com>
2023-12-17 14:09:32 +0100

    change default values and check logic & add name and allow_unref_chars in body

--- src/routes/create.py
+++ src/routes/create.py
@@ -1,76 +1,71 @@
+from typing import Optional
+
 from pydantic import BaseModel
 from fastapi import APIRouter, Body
 from fastapi.responses import JSONResponse
 
-from database.models import EncodingType
+from database.models import EncodingStandard
 from database.connection import LocalSession
 
-class EncodingTypeModel(BaseModel):
-    encoding_chars: dict[str, str]
-    is_case_sensitive: bool = False
-    encoded_chars_sep: str = ""
-    encoded_words_sep: str = ""
+class EncodingStandardModel(BaseModel):
+    name: str
+    charset: dict[str, str]
+    case_sensitive: Optional[bool] = True
+    allowed_unrefenced_chars: Optional[bool] = False
+
+    encoded_char_len: Optional[int] = None
+    encoded_char_sep: Optional[str] = ""
+    encoded_word_sep: Optional[str] = " "
 
 router = APIRouter()
 
-@router.post("/encodex/{encoding_type_name}/create/")
-def create_encoding_type(encoding_type_name: str, new_encoding_type: EncodingTypeModel = Body(...)):
+@router.post("/encodex/create/")
+def create_encoding_standard(encoding_standard: EncodingStandardModel = Body(...)):
 
     session = LocalSession()
 
-    if(session.query(EncodingType).filter(EncodingType.name == encoding_type_name).first() is not None):
+    if(session.query(EncodingStandard).filter(EncodingStandard.name == encoding_standard.name).first() is not None):
         session.close()
-        return JSONResponse(status_code=409, content={"Succes": False, "message": "Encoding type already exists"})
+        return JSONResponse(status_code=409, content={"Succes": False, "message": f"Character encoding standard named '{encoding_standard.name}' already exists"})
 
-    if(not new_encoding_type.is_case_sensitive):
-        for char in new_encoding_type.encoding_chars.keys():
-            new_encoding_type.encoding_chars[char] = new_encoding_type.encoding_chars[char].lower()
+    if(not encoding_standard.case_sensitive):
+        for char in encoding_standard.charset.keys():
+            encoding_standard.charset[char] = encoding_standard.charset[char].lower()
 
-    encoded_chars_lens = set()
-    for char, encoded_char in new_encoding_type.encoding_chars.items():
-        if(len(char) != 1):
-            session.close()
-            return JSONResponse(status_code=422, content={"succes": False, "message": "Encoding characters keys must be one character long"})
-        if(len(encoded_char) == 0):
-            session.close()
-            return JSONResponse(status_code=422, content={"succes": False, "message": "Encoded characters cannot be empty"})
-        if(new_encoding_type.encoded_chars_sep != "" and new_encoding_type.encoded_chars_sep in encoded_char or new_encoding_type.encoded_words_sep != "" and new_encoding_type.encoded_words_sep in encoded_char):
-            session.close()
-            return JSONResponse(status_code=422, content={"succes": False, "message": "Encoded characters cannot contain separators"})
 
-        encoded_chars_lens.add(len(encoded_char))
-
-    if(len(new_encoding_type.encoding_chars.values()) != len(set(new_encoding_type.encoding_chars.values()))):
+    if(len(encoding_standard.charset.values()) != len(set(encoding_standard.charset.values()))):
         session.close()
         return JSONResponse(status_code=422, content={"succes": False, "message": "Encoded characters must be unique"})
 
-    if(new_encoding_type.encoded_chars_sep == new_encoding_type.encoded_words_sep and new_encoding_type.encoded_chars_sep != ""):
+    if(encoding_standard.encoded_char_sep == encoding_standard.encoded_word_sep and encoding_standard.encoded_char_sep != ""):
         session.close()
-        return JSONResponse(status_code=422, content={"succes": False, "message": "Encoded characters and encoded words separators cannot be the same"})
+        return JSONResponse(status_code=422, content={"succes": False, "message": "Encoded characters and encoded words separators cannot be equal"})
 
-    if(len(encoded_chars_lens) > 1):
-        if(new_encoding_type.encoded_chars_sep and new_encoding_type.encoded_words_sep):
-            encoded_char_len = None
-        else:
+    for char, encoded_char in encoding_standard.charset.items():
+        if(len(char) != 1):
             session.close()
-            return JSONResponse(
-                status_code = 422,
-                content = {
-                    "succes": False,
-                    "message": "Encoded characters have different lengths, "
-                        + "please specify encoded characters and encoding words separators"
-                        + "or make all encoded characters have the same length"
-                })
-    else:
-        encoded_char_len = encoded_chars_lens
+            return JSONResponse(status_code=422, content={"succes": False, "message": "Characters must be one character long"})
 
-    session.add(EncodingType(
-        name = encoding_type_name,
-        encoding_chars = new_encoding_type.encoding_chars,
-        is_case_sensitive = new_encoding_type.is_case_sensitive,
-        encoded_chars_sep = new_encoding_type.encoded_chars_sep,
-        encoded_words_sep = new_encoding_type.encoded_words_sep,
-        encoded_chars_len = encoded_char_len
+        if(encoding_standard.encoded_char_len is None):
+            if(len(encoded_char) == 0):
+                session.close()
+                return JSONResponse(status_code=422, content={"succes": False, "message": "Encoded characters cannot be empty"})
+        elif(len(encoded_char) != encoding_standard.encoded_char_len):
+            session.close()
+            return JSONResponse(status_code=422, content={"succes": False, "message": "Encoded characters must have the same length as specified in the encoding standard"})
+
+        if(encoding_standard.encoded_char_sep != "" and encoding_standard.encoded_char_sep in encoded_char or encoding_standard.encoded_word_sep != "" and encoding_standard.encoded_word_sep in encoded_char):
+            session.close()
+            return JSONResponse(status_code=422, content={"succes": False, "message": "Encoded characters cannot contain separators"})
+
+    session.add(EncodingStandard(
+        name = encoding_standard.name,
+        charset = encoding_standard.charset,
+        case_sensitive = encoding_standard.case_sensitive,
+        allowed_unrefenced_chars = encoding_standard.allowed_unrefenced_chars,
+        encoded_chars_len = encoding_standard.encoded_char_len,
+        encoded_chars_sep = encoding_standard.encoded_char_sep,
+        encoded_words_sep = encoding_standard.encoded_word_sep
     ))
 
     session.commit()
