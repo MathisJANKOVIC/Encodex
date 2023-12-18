8ddd3cdfe69ebb7bc1b9ecaa1be0ce6ed28a660e
MathisJANKOVIC <mathis.jankovic@gmail.com>
2023-12-18 14:27:57 +0100

    update names, routes and structure of encode.py

--- src/routes/encode.py
+++ src/routes/encode.py
@@ -2,36 +2,46 @@ from pydantic import BaseModel
 from fastapi import APIRouter, Body
 from fastapi.responses import JSONResponse
 
-from database.models import EncodingType
+from database.models import EncodingStandard
 from database.connection import LocalSession
 
 class BodyModel(BaseModel):
+    encoding_standard_name: str
     string: str
 
 router = APIRouter()
 
-@router.post("/encodex/{encoding_type_name}/encode/")
-def encode_string(encoding_type_name: str, body: BodyModel = Body(...)):
+@router.post("/encodex/encode/")
+def encode_string(body: BodyModel = Body(...)):
 
     session = LocalSession()
-    encoding_type: EncodingType = session.query(EncodingType).filter(EncodingType.name == encoding_type_name).first()
+    encoding_standard: EncodingStandard = session.query(EncodingStandard).filter(EncodingStandard.name == body.encoding_standard_name).first()
 
-    if(encoding_type is None):
+    if(encoding_standard is None):
         session.close()
-        return JSONResponse(status_code=404, content={"succes": False, "message": "Encoding type not found"})
+        return JSONResponse(status_code=404, content={"succes": False, "message": "Character encoding standard not found"})
 
     encoded_string = ""
     for i, char in enumerate(body.string):
-        if(not encoding_type.is_case_sensitive):
+        if(not encoding_standard.case_sensitive):
             char = char.lower()
 
         if(char == " "):
-            encoded_string = encoded_string + encoding_type.encoded_words_sep
+            encoded_string = encoded_string + encoding_standard.encoded_word_sep
         else:
-            encoded_string = encoded_string + encoding_type.encoded_char(char)
+            encoded_char = encoding_standard.encode_char(char)
+
+            if(encoded_char is None):
+                if(encoding_standard.allowed_unrefenced_chars):
+                    encoded_string = encoded_string + char
+                else:
+                    session.close()
+                    return JSONResponse(status_code=422, content={"succes": False, "message": f"Coulnd't encode character `{char}`"})
+            else:
+                encoded_string = encoded_string + encoded_char
 
             if(i+1 < len(body.string) and body.string[i+1] != " "): # If the next character is not a space or the last character
-                encoded_string = encoded_string + encoding_type.encoded_chars_sep
+                encoded_string = encoded_string + encoding_standard.encoded_char_sep
 
     session.close()
     return JSONResponse(status_code=200, content={"succes": True, "content": encoded_string})
