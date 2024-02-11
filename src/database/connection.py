fb672ba316c66c1672971c7973b98b663b2758d8
MathisJANKOVIC <mathis.jankovic@gmail.com>
2024-02-11 13:25:13 +0100

    Add environment variables for db connection

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,12 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+import os
+
 DRIVERS = "mysql+pymysql"
-USER = "root"
-PASSWORD = ""
-HOST = "db"
-PORT = "3306"
-DATABASE = "encodex"
+
+HOST = os.environ.get("DB_HOST", "localhost")
+PORT = os.environ.get("DB_PORT", "3306")
+
+USER = os.environ.get("DB_USER", "root")
+PASSWORD = os.environ.get("DB_PASSWORD", "")
+
+DATABASE = os.environ.get("DB_NAME", "encodex")
 
 engine = create_engine(f"{DRIVERS}://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
 LocalSession = sessionmaker(bind=engine)
